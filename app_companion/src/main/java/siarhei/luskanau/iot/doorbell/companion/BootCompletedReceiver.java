696914d29a840aac646d1f364d027e22fef80ab3
Siarhei_Luskanau <siarhei,luskanau@gmail.com>
2017-02-17 14:31:35 +0300

    aurorun

---
+++ app_companion/src/main/java/siarhei/luskanau/iot/doorbell/companion/BootCompletedReceiver.java
@@ -0,0 +1,17 @@
+package siarhei.luskanau.iot.doorbell.companion;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = BootCompletedReceiver.class.getSimpleName();
+
+    public BootCompletedReceiver() {
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, intent.getAction());
+    }
+}
