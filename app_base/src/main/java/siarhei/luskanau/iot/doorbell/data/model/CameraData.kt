3d3cc6e3477da2caef1c645e9668217d35ad9fc1
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2018-03-13 00:12:19 +0300

    Added camera info into CameraData

--- app_base/src/main/java/siarhei/luskanau/iot/doorbell/data/model/CameraData.kt
+++ app_base/src/main/java/siarhei/luskanau/iot/doorbell/data/model/CameraData.kt
@@ -2,4 +2,7 @@ package siarhei.luskanau.iot.doorbell.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class CameraData(@SerializedName("camera_id") val cameraId: String)
+data class CameraData(
+        @SerializedName("camera_id") val cameraId: String,
+        @SerializedName("info") val info: Map<String, Any>
+)
