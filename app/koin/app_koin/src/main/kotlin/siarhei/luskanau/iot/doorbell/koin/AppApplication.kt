6e5a714e202598e8d15fb288a357e4d7e5b3e789
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2021-08-17 20:48:32 +0300

    Added ndroidx.startup.Initializer for Koim

--- app/koin/app_koin/src/main/kotlin/siarhei/luskanau/iot/doorbell/koin/AppApplication.kt
+++ app/koin/app_koin/src/main/kotlin/siarhei/luskanau/iot/doorbell/koin/AppApplication.kt
@@ -4,20 +4,9 @@ import android.app.Application
 import androidx.fragment.app.FragmentActivity
 import androidx.work.WorkerFactory
 import org.koin.android.ext.android.get
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidFileProperties
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 import siarhei.luskanau.iot.doorbell.data.AppBackgroundServices
 import siarhei.luskanau.iot.doorbell.data.ScheduleWorkManagerService
-import siarhei.luskanau.iot.doorbell.koin.di.appModule
 import siarhei.luskanau.iot.doorbell.koin.di.getFragmentFactory
-import siarhei.luskanau.iot.doorbell.koin.doorbelllist.di.doorbellListModule
-import siarhei.luskanau.iot.doorbell.koin.imagedetails.di.imageDetailsModule
-import siarhei.luskanau.iot.doorbell.koin.imagelist.di.imageListModule
-import siarhei.luskanau.iot.doorbell.koin.permissions.di.permissionsModule
-import siarhei.luskanau.iot.doorbell.koin.splash.di.splashModule
 import siarhei.luskanau.iot.doorbell.navigation.OnActivityCreatedLifecycleCallbacks
 import siarhei.luskanau.iot.doorbell.workmanager.DefaultWorkerFactory
 import siarhei.luskanau.iot.doorbell.workmanager.WorkerFactoryProvider
@@ -28,22 +17,6 @@ class AppApplication : Application(), WorkerFactoryProvider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-
-        startKoin {
-            androidLogger(Level.ERROR)
-            androidContext(this@AppApplication)
-            androidFileProperties()
-            modules(
-                listOf(
-                    appModule,
-                    doorbellListModule,
-                    imageDetailsModule,
-                    imageListModule,
-                    permissionsModule,
-                    splashModule
-                )
-            )
-        }
 
         get<ScheduleWorkManagerService>().startUptimeNotifications()
         get<AppBackgroundServices>().startServices()
