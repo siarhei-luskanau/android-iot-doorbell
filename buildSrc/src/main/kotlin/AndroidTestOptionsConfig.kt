f2dc7ee39f9d2d1712d67af92d8df0ff6b325450
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2024-12-31 08:07:41 +0100

    Refactor: Include all test log events in Android test options

--- buildSrc/src/main/kotlin/AndroidTestOptionsConfig.kt
+++ buildSrc/src/main/kotlin/AndroidTestOptionsConfig.kt
@@ -10,14 +10,7 @@ fun TestOptions.configureAndroidTestOptions() {
         all { test: org.gradle.api.tasks.testing.Test ->
             test.testLogging {
                 exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-                events = setOf(
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
-                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-                )
+                events = org.gradle.api.tasks.testing.logging.TestLogEvent.values().toSet()
             }
         }
     }
