9d3b307f19c649fc4cb2b067051d3134a287dff0
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-06 07:14:33 +0100

    Refactor: Configure emulator versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.util.Properties
 import org.apache.tools.ant.taskdefs.condition.Os
 
 println("gradle.startParameter.taskNames: ${gradle.startParameter.taskNames}")
-System.getProperties().forEach { key, value -> println("System.getProperties(): $key=$value") }
+System.getProperties().forEach { (key, value) -> println("System.getProperties(): $key=$value") }
 System.getenv().forEach { (key, value) -> println("System.getenv(): $key=$value") }
 
 plugins {
--- buildSrc/src/main/kotlin/AndroidTestOptionsConfig.kt
+++ buildSrc/src/main/kotlin/AndroidTestOptionsConfig.kt
@@ -1,6 +1,6 @@
 import com.android.build.api.dsl.TestOptions
 
-val EMULATOR_VERSIONS = 30..36
+val EMULATOR_VERSIONS = listOf(27, 29, 31, 34, 36)
 
 fun TestOptions.configureAndroidTestOptions() {
     unitTests {
@@ -8,14 +8,11 @@ fun TestOptions.configureAndroidTestOptions() {
         all { test: org.gradle.api.tasks.testing.Test ->
             test.testLogging {
                 exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-                events = org.gradle.api.tasks.testing.logging.TestLogEvent.values().toSet()
+                events = org.gradle.api.tasks.testing.logging.TestLogEvent.entries.toSet()
             }
         }
     }
     animationsDisabled = true
-    emulatorSnapshots {
-        enableForTestFailures = false
-    }
     EMULATOR_VERSIONS.forEach { version ->
         managedDevices.localDevices.create("managedVirtualDevice$version") {
             device = "Pixel 2"
